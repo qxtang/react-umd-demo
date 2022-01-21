b9c62a017651dbaded2f918c46cb1c69daee259e
tangqx01 <tangqx01@mingyuanyun.com>
2022-01-21 17:58:37 +0800

    save

---
+++ .babelrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    'presets': [
+        '@babel/preset-react',
+        '@babel/typescript'
+    ],
+
+    // antd4 已默认支持按需加载
+    // 'plugins': [
+    //     [
+    //         'import',
+    //         {
+    //             'libraryName': 'antd',
+    //             // 'libraryDirectory': 'es',
+    //             'style': true
+    //         }
+    //     ]
+    // ]
+};
