4065cdc25f4e74d84e07f38689b0716f4607cd19
tangqx01 <tangqx01@mingyuanyun.com>
2022-02-10 12:06:56 +0800

    publish

---
+++ typings/qwsdk-react.d.ts
@@ -0,0 +1,9 @@
+import * as React from 'react';
+import * as types from '../typings';
+
+declare class QwSdk extends React.Component<types.default.RenderOptions> {
+  constructor(props: types.default.RenderOptions);
+  render(): JSX.Element;
+}
+
+export default QwSdk;
--- webpack.config.react.js
+++ webpack.config.react.js
@@ -1,10 +1,19 @@
 // 用于打包 react 组件的配置
 
+const path = require('path');
 const webpackConfig = Object.create(require('./webpack.config.js'));
+const CopyPlugin = require('copy-webpack-plugin');
 
 webpackConfig.entry = './Main.tsx';
 webpackConfig.output = Object.create(webpackConfig.output);
 webpackConfig.output.filename = 'qwsdk-react.js';
+
+webpackConfig.plugins = [
+  ...webpackConfig.plugins,
+  new CopyPlugin({
+    patterns: [{ from: path.resolve(__dirname, 'typings/qwsdk-react.d.ts'), to: path.resolve(__dirname, 'dist') }]
+  })
+];
 
 // 使用 commonjs 规范
 webpackConfig.externals = {
